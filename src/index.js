fe84c335a193090057e3d80a1d1d16dbd785b5be
Rahul Mayuranath <rahulmr41@gmail.com>
2017-07-07 15:00:51 +0530

    Setup eslint babel and react-router

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+
+import Home from './pages/index';
+import Other from './pages/other';
+
+// render on page
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/other">Other</Link></li>
+            </ul>
+
+            <hr />
+
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/other" component={Other} />
+                <Route component={() => <div><h1>Select route</h1></div>} />
+            </Switch>
+        </div>
+    </BrowserRouter>,
+    document.getElementById('app')
+);
